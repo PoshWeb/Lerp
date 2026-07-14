13c7280eaf7c9e7859946ddc9ffe2ceabc27763e
James Brundage <+@noreply.github.com>
2026-07-14 16:36:34 -0700

    docs: `Lerp.ps1` ( Fixes #2 )
    Adding inner docs

--- Lerp.ps1
+++ Lerp.ps1
@@ -21,34 +21,38 @@
 param(
 # The start point
 [PSObject]
-$Start = 0,
+$Start,
 
 # The end point
 [PSObject]
-$End = 1,
+$End,
 
 # The amount of lerp
 # The percentage of change between two points
-[Alias('Change')]
+[Alias('Change','Percent')]
 [float]
-$Amount = 0.5
+$Amount
 )
 
+# If either start or end is empty, default them
 if (-not $start) { $start = 0 }
 if (-not $end) { $end = 0 }
 
+# Check that objects are the same type
 if ($start.GetType() -ne $end.GetType()) {
     throw "Objects must be the same type"
 }
 
-$mySelf = $MyInvocation.MyCommand.ScriptBlock
-
+# Handle arrays
 if ($start -is [object[]]) {
+    # by trying to turn them into numbers.
     $startVector = $start -as [float[]]
     $endVector = $end -as [float[]]
+    # Then check our vector lengths match.
     if ($startVector.Length -ne $endVector.Length) {
         throw "Arrays must be the same length"
-    }        
+    }
+    # Then handle each potential dimension
     if ($startVector.Length -eq 1) {
         $start = [Numerics.Vector2]::new($startVector[0], 1)
         $end = [Numerics.Vector2]::new($endVector[0], 1)
@@ -62,29 +66,38 @@ if ($start -is [object[]]) {
         $start = [Numerics.Vector4]::new($startVector)
         $end = [Numerics.Vector4]::new($endVector)
     } else {
+        # or throw an exception because our array is too large.
         throw "Too big to lerp: Array must be 1-4 elements, not $($startVector.Length) : $startVector"      
     }
 }
 
+# If the start or end is an int or double, make them floats.
 if ($start -is [int] -or $start -is [double]) {$Start = $start -as [float]}
 if ($end -is [int] -or $end -is [double]) {$End = $End -as [float]}
 
+
+# 1D Lerp
 if ($start -is [float] -and $end -is [float]) {
     $start = [Numerics.Vector2]::new($start,0)
     $end = [Numerics.Vector2]::new($end,0)
     return [Numerics.Vector2]::Lerp($Start, $End, $Amount).X
 }
 
-if ($Start -is [Numerics.Vector2]) {
+# 2D Lerp
+if ($Start -is [Numerics.Vector2]) {    
     return [Numerics.Vector2]::Lerp($Start, $End, $Amount)
 }
 
+
+# 3D Lerp
 if ($Start -is [Numerics.Vector3]) {
     return [Numerics.Vector3]::Lerp($Start, $End, $Amount)
 }
 
+# 4D Lerp
 if ($Start -is [Numerics.Vector4]) {
     return [Numerics.Vector4]::Lerp($Start, $End, $Amount)
 }
 
+# IF it's none of these things, throw.
 throw "Invalid type $($Start.GetType().Fullname):  Must be one of -" + "int, float, double, or Numerics.Vector2/3/4"
